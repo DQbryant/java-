a17772707d4a3168b2dbf6bde8421c6c5c8fb20e
DQbryant <45484155+DQbryant@users.noreply.github.com>
2019-11-17 15:56:29 +0800

    Delete Shop.java

--- Shop.java
+++
@@ -1,9 +0,0 @@
-package milkTea;
-
-/**
- *
- */
-public interface Shop {
-    void addProduction(Ingredient ingredient);
-    MilkTea sellMilkTea(String name,String ingredientName) throws SoldOutException;
-}
